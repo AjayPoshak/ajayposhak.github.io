caf0dc184dabcb3dd6d9ec0198e191a3cf2dc771
Ajay Poshak <poshakajay@gmail.com>
2018-05-20 21:32:31 +0530

    UI updates

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 (function() {
+  const videoURLs = [{
+        type: 'mp4',
+        URL: 'http://res.cloudinary.com/ddbxa4afa/video/upload/ac_none/v1526734008/WhatsApp_Video_2018-05-19_at_3.14.30_PM_k6f5aw.mp4'
+    }, {
+        type: 'webm',
+        URL: 'http://res.cloudinary.com/ddbxa4afa/video/upload/ac_none,c_scale,q_auto:eco,w_414/v1526734008/WhatsApp_Video_2018-05-19_at_3.14.30_PM_k6f5aw.webm'
+    }];
   const sideBarBtn = document.getElementById("js-sidebar-btn"),
-    sideBar = document.getElementById("js-sideBar");
+    sideBar = document.getElementById("js-sideBar"),
+    videoElement = document.getElementsByTagName('video')
 
   sideBarBtn.addEventListener("click", clickHandler, false);
   sideBar.addEventListener("click", clickHandler, false);
@@ -14,4 +22,13 @@
       sideBar.classList.add("close-sidebar");
     }
   }
+
+  if (videoElement) {
+      for (let i = 0; i < videoURLs.length; i++) {
+          const source = document.createElement('source');
+          source.src = videoURLs[i].URL;
+          source.type = `video/${videoURLs[i].type}`
+          videoElement.appendChild(source);
+      }
+    }
 })();
