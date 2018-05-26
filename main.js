910814021c7da802ab651b3894893eaaea0ab164
Ajay Poshak <poshakajay@gmail.com>
2018-05-27 01:01:01 +0530

    Desktop UI Changes

--- main.js
+++ main.js
@@ -1,14 +1,37 @@
 (function() {
-  const videoURLs = [{
-        type: 'mp4',
-        URL: 'http://res.cloudinary.com/ddbxa4afa/video/upload/ac_none/v1526734008/WhatsApp_Video_2018-05-19_at_3.14.30_PM_k6f5aw.mp4'
-    }, {
-        type: 'webm',
-        URL: 'http://res.cloudinary.com/ddbxa4afa/video/upload/ac_none,c_scale,q_auto:eco,w_414/v1526734008/WhatsApp_Video_2018-05-19_at_3.14.30_PM_k6f5aw.webm'
-    }];
+  const md = new MobileDetect(window.navigator.userAgent),
+    isMobile = md.mobile() || false;
+
+  const videoURLs = {
+    mobile: [
+      {
+        type: "mp4",
+        URL:
+          "http://res.cloudinary.com/ddbxa4afa/video/upload/ac_none,c_scale,h_450,q_auto:eco,w_434/v1526734008/WhatsApp_Video_2018-05-19_at_3.14.30_PM_k6f5aw.mp4"
+      },
+      {
+        type: "webm",
+        URL:
+          "http://res.cloudinary.com/ddbxa4afa/video/upload/ac_none,c_scale,h_450,q_auto:eco,w_434/v1526734008/WhatsApp_Video_2018-05-19_at_3.14.30_PM_k6f5aw.webm"
+      }
+    ],
+    desktop: [
+      {
+        type: "mp4",
+        URL:
+          "http://res.cloudinary.com/ddbxa4afa/video/upload/ac_none/v1526734008/WhatsApp_Video_2018-05-19_at_3.14.30_PM_k6f5aw.mp4"
+      },
+      {
+        type: "webm",
+        URL:
+          "http://res.cloudinary.com/ddbxa4afa/video/upload/ac_none,c_scale,h_450,q_auto:eco,w_434/v1526734008/WhatsApp_Video_2018-05-19_at_3.14.30_PM_k6f5aw.webm"
+      }
+    ]
+  };
+
   const sideBarBtn = document.getElementById("js-sidebar-btn"),
     sideBar = document.getElementById("js-sideBar"),
-    videoElement = document.getElementsByTagName('video')
+    videoParent = document.getElementById("js-video");
 
   sideBarBtn.addEventListener("click", clickHandler, false);
   sideBar.addEventListener("click", clickHandler, false);
@@ -23,12 +46,15 @@
     }
   }
 
-  if (videoElement) {
-      for (let i = 0; i < videoURLs.length; i++) {
-          const source = document.createElement('source');
-          source.src = videoURLs[i].URL;
-          source.type = `video/${videoURLs[i].type}`
-          videoElement.appendChild(source);
-      }
+  if (videoParent) {
+    videoElement = document.createElement("video");
+    const videoLinks = isMobile ? videoURLs.mobile : videoURLs.desktop;
+    for (let i = 0; i < videoLinks.length; i++) {
+      const source = document.createElement("source");
+      source.src = videoLinks[i].URL;
+      source.type = `video/${videoLinks[i].type}`;
+      videoElement.appendChild(source);
     }
+    videoParent.appendChild(videoElement);
+  }
 })();
